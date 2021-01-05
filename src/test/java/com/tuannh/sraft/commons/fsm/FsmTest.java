1b805a96a750fc369ce49b0476cd75652d79d03d
tuannh29 <tuannh27@viettel.com.vn>
2021-01-05 11:46:44 +0700

    fsm test

---
+++ src/test/java/com/tuannh/sraft/commons/fsm/FsmTest.java
@@ -0,0 +1,60 @@
+package com.tuannh.sraft.commons.fsm;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class FsmTest {
+    @ToString
+    @AllArgsConstructor
+    static class Order implements FsmEntity {
+        private final String name;
+        private State state;
+
+        @Override
+        public State state() {
+            return state;
+        }
+
+        @Override
+        public void changeState(State newState) {
+            state = newState;
+        }
+    }
+
+    @Test
+    public void simpleTest() {
+        // declare states
+        State newOrder = State.of("new-order");
+        State processingOrder = State.of("processing-order");
+        State doneOrder = State.of("done-order");
+        // declare events
+        Event process = Event.of("process");
+        Event done = Event.of("done");
+        // entries
+        TransitionEntry new2processing = new TransitionEntry(
+                newOrder,
+                process,
+                processingOrder,
+                entity -> System.out.println(entity + " new2processing called")
+        );
+        TransitionEntry processing2done = new TransitionEntry(
+                processingOrder,
+                done,
+                doneOrder,
+                entity -> System.out.println(entity + " processing2done called")
+        );
+        FSM fsm = new FSM(
+                Arrays.asList(new2processing, processing2done)
+        );
+        Order x = new Order("order1", newOrder);
+        fsm.transition(x, process);
+        assertEquals(x.state(), processingOrder);
+        fsm.transition(x, done);
+        assertEquals(x.state(), doneOrder);
+    }
+}
